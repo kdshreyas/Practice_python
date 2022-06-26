f75bfdb13b7cbd2be6cf8d99eb753967bc7cd011
Shreyas <87318121+shreskulk@users.noreply.github.com>
2022-06-26 14:22:33 +0530

    added .py file

---
+++ operations.py
@@ -0,0 +1,9 @@
+n = int(input("Input a number: "))
+sum_num = (n * (n + 1)) / 2
+print("Sum of the first", n ,"positive integers:", sum_num)
+
+
+
+n = int(input("Input a number: "))
+sum_num = (n * (n + 1)) / 2
+print("Sum of the first", n ,"positive integers:", sum_num)

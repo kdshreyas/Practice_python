0817bbf3301f4e15ab46d05a088571085f0d51db
Shreyas <87318121+shreskulk@users.noreply.github.com>
2022-06-26 14:38:40 +0530

    lets check how we can merge

--- operations.py
+++ operations.py
@@ -7,3 +7,5 @@ print("Sum of the first", n ,"positive integers:", sum_num)
 n = int(input("Input a number: "))
 sum_num = (n * (n + 1)) / 2
 print("Sum of the first", n ,"positive integers:", sum_num)
+
+print('This is the new change i am doing to understand the how git merge works!')
